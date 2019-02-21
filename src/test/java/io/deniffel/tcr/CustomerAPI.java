661c275376ef2859181e65e4c4d783e2bcb9ed3e
Thomas Deniffel <tdeniffel@acm.org>
2019-02-21 13:12:05 +0100

    working

--- src/test/java/io/deniffel/tcr/CustomerAPI.java
+++ src/test/java/io/deniffel/tcr/CustomerAPI.java
@@ -1,8 +1,16 @@
 package io.deniffel.tcr;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CustomerAPI {
 
     public void create(Object o) {
         throw new IllegalArgumentException();
+    }
+
+    public List<Object> all() {
+        return new LinkedList<Object>();
     }
 }
--- src/test/java/io/deniffel/tcr/CustomerAPITests.java
+++ src/test/java/io/deniffel/tcr/CustomerAPITests.java
@@ -1,11 +1,19 @@
 package io.deniffel.tcr;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CustomerAPITests {
 
     @Test(expected = IllegalArgumentException.class)
     public void creatingCustomerWithNull_Throws() {
         new CustomerAPI().create(null);
+    }
+
+    @Test
+    public void allCustomers_initialNoElements() {
+        assertEquals(0, new CustomerAPI().all().size());
     }
 }
