fa552410b2b23a240c4c3f838be6f81751590a60
Thomas Deniffel <tdeniffel@acm.org>
2019-02-21 13:35:51 +0100

    working

--- src/main/java/io/deniffel/tcr/Customer.java
+++ src/main/java/io/deniffel/tcr/Customer.java
@@ -1,4 +1,7 @@
 package io.deniffel.tcr;
 
 public class Customer {
+    public boolean isValid() {
+        return false;
+    }
 }
--- src/test/java/io/deniffel/tcr/CustomerValidationTest.java
+++ src/test/java/io/deniffel/tcr/CustomerValidationTest.java
@@ -1,4 +1,14 @@
 package io.deniffel.tcr;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CustomerValidationTest {
+
+    @Test
+    public void emptyCustomer_notValid() {
+        assertFalse(new Customer().isValid());
+    }
 }
