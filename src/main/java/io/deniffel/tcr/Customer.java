1469708981e55c569fccf0da822ad9737b3ed249
Thomas Deniffel <tdeniffel@acm.org>
2019-02-21 13:37:02 +0100

    working

--- src/main/java/io/deniffel/tcr/Customer.java
+++ src/main/java/io/deniffel/tcr/Customer.java
@@ -1,7 +1,18 @@
 package io.deniffel.tcr;
 
 public class Customer {
+
+    private String name;
+
     public boolean isValid() {
         return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
