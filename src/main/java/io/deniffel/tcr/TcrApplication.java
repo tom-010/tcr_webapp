08d795f231c84377b2a6accbef57013684fa526f
Thomas Deniffel <tdeniffel@acm.org>
2019-02-21 12:49:25 +0100

    init

---
+++ src/main/java/io/deniffel/tcr/TcrApplication.java
@@ -0,0 +1,13 @@
+package io.deniffel.tcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/deniffel/tcr/TcrApplicationTests.java
@@ -0,0 +1,16 @@
+package io.deniffel.tcr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TcrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
